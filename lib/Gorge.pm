234eca3dbbbd08cf8b1b8571658c401c5ca9f139
Jason A. Crome <jason@crome-plated.com>
2024-08-22 22:40:05 -0400

    Pull questions from the database

--- lib/Gorge.pm
+++ lib/Gorge.pm
@@ -1,13 +1,32 @@
 package Gorge;
 use Dancer2;
+use Dancer2::Plugin::Database;
+use Data::Printer;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
     # Make sure we don't keep any choices from last time
-    #app->destroy_session;
+    app->destroy_session;
 
-    template 'index';
+    # quick_select mangles our order by. Boo!
+    my $sth = database->prepare( qq{
+        SELECT question_id, question
+        FROM questions
+        ORDER BY RANDOM()
+        LIMIT 1
+    });
+    $sth->execute;
+    my $question = $sth->fetchrow_hashref;
+    my @answers = database->quick_select(
+        'answers',
+        { question_id => $question->{ question_id } },
+    );
+
+    template 'index', {
+        gotcha  => $question,
+        answers => \@answers,
+    };
 };
 
 post "/quest" => sub {
