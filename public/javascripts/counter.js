25a6e209f1ed3efbf882f1b23f97b36c67c66ef9
Jason A. Crome <jason@crome-plated.com>
2024-08-23 14:21:08 -0400

    Renamed the select, but not the JS. Oops.

--- public/javascripts/counter.js
+++ public/javascripts/counter.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var counter = 0;
-    $('#answers').on( 'click', function() {
+    $('#answer').on( 'click', function() {
         counter++;
         if( counter > 1 ) {
             location.href = '/into-the-gorge';
